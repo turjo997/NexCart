2d2594707a13a8e022a464c38c857a3e4f5388dc
turjo997 <turja997@gmail.com>
2025-12-29 15:35:48 +0600

    project setup done

---
+++ src/main/java/com/spring/NexCart/NexCartApplication.java
@@ -0,0 +1,13 @@
+package com.spring.NexCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexCartApplication.class, args);
+	}
+
+}
